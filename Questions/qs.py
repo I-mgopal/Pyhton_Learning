fa3996e73403c96057a71696a21068a3c24219cb
Gopal Mondal <gopalmandol439@gmail.com>
2025-10-12 22:04:21 +0530

    Updated in Qs

--- Questions/qs.py
+++ Questions/qs.py
@@ -1,9 +1,14 @@
-#Q16 GCD
-
+#Q16 Return GCD and LCM
 def GCD(a,b):
     while b:
         a, b = b, a%b
     return a
     
+#Q17 LCM
+def LCM(a,b):
+    lcm = int((a*b)/GCD(a,b))
+    return lcm
 
 print(f"GCD is {GCD(6,2)}")
+print(f"LCM is {LCM(6,2)}")
+
